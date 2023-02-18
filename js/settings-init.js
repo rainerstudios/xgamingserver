f85ed01446d2c3de14c914bf68c818ada45acb3c
Rainer Mbongo <rainermbongo@gmail.com>
2023-02-18 10:43:46 +0000

    Add files via upload

---
+++ js/settings-init.js
@@ -0,0 +1,12 @@
+
+"use strict"
+var optionSettings = {
+    layout:"wide",
+    background:"dark",
+    color:"green",
+    header:"fixed",
+    font:"opensans",
+    textDirection:"ltr",
+    showSettings:"hide",
+};
+new antlerSettings(optionSettings);
